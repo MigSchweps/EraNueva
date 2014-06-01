721d56c5f549f9a345c817b00ecfccf17d836879
Clovis <clovis.tiague@gmail.com>
2014-06-01 15:37:45 +0200

    Resize template

--- wp-content/themes/mh-magazine-lite/functions.php
+++ wp-content/themes/mh-magazine-lite/functions.php
@@ -67,7 +67,7 @@ function mh_themes_setup() {
 	add_theme_support('automatic-feed-links');
 	add_theme_support('custom-background');		
 	add_theme_support('post-thumbnails');
-	add_image_size('content', 620, 264, true);
+	add_image_size('content', 700, 270, true);
 	add_image_size('loop', 174, 131, true);
 	add_image_size('cp_small', 70, 53, true);
 	add_editor_style();
